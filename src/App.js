14b98dd45f67a2bf9273d21a5d6772f39ee73452
Noor Haj Dawood <noorhajdawood@gmail.com>
2023-02-27 21:02:09 +0200

    conflict merges

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BarChart } from "./cmp/bar-chart/bar-chart";
 import { AreaChart } from "./cmp/line-chart/line-chart";
+import { Navbar } from "./cmp/navbar/navbar";
 import { DisplayGraph } from "./cmp/network-graph/networkGraph";
 import { PieChart } from "./cmp/pie/pie";
+import { MultiTableDrag } from "./cmp/table/table";
 import { FormNewProject } from "./pages/form-new-project/form";
 import { LoginPage } from "./pages/login-signup/loginSignup";
-import { Timerange } from "./pages/timerange/timerange";
-import { Navbar } from "./cmp/navbar/navbar";
-<<<<<<< Updated upstream
 import { Profile } from "./pages/profile/profile";
-
-=======
-import { MultiTableDrag } from "./cmp/table/table";
->>>>>>> Stashed changes
+import { Timerange } from "./pages/timerange/timerange";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/addProject" element={<FormNewProject />} />
@@ -38,13 +34,13 @@ function App() {
               <AreaChart width={600} height={600} dataArray={appleStock} />
             }
           />
-          <Route path="/networkGraph" element={<DisplayGraph width="100vw" height="100vh"/>} />
+          <Route
+            path="/networkGraph"
+            element={<DisplayGraph width="100vw" height="100vh" />}
+          />
           <Route path="/timeRange" element={<Timerange />} />
-<<<<<<< Updated upstream
-          <Route path="/profile" element={<Profile/>} />
-=======
+          <Route path="/profile" element={<Profile />} />
           <Route path="/table" element={<MultiTableDrag />} />
->>>>>>> Stashed changes
         </Routes>
       </div>
     </BrowserRouter>
