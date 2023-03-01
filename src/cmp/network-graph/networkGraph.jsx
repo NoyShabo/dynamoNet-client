59ff25e2985e2be0dc7e2ecf8de8e3be037384bb
Noor Haj Dawood <noorhajdawood@gmail.com>
2023-03-01 12:56:44 +0200

    updated mock project

--- src/cmp/network-graph/networkGraph.jsx
+++ src/cmp/network-graph/networkGraph.jsx
@@ -3,6 +3,7 @@ import "@react-sigma/core/lib/react-sigma.min.css";
 import { useWorkerLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
 import Graph from "graphology";
 import { useEffect } from "react";
+// import Sigma from "sigma";
 import "./networkGraph.scss";
 
 export const LoadGraph = ({ network }) => {
@@ -16,8 +17,89 @@ export const LoadGraph = ({ network }) => {
   const { start, kill } = useWorkerLayoutForceAtlas2({
     settings: { slowDown: 15 },
   });
+  // const container = document.getElementById("sigma-container");
+
   useEffect(() => {
     const graph = new Graph();
+    // const renderer = new Sigma(graph, container);
+    // const state = {
+    //   hoveredNode: undefined,
+    //   searchQuery: "",
+    //   selectedNode: undefined,
+    //   suggestions: undefined,
+    //   hoveredNeighbors: undefined,
+    // };
+
+    // function setHoveredNode(node) {
+    //   if (node) {
+    //     state.hoveredNode = node;
+    //     state.hoveredNeighbors = new Set(graph.neighbors(node));
+    //   } else {
+    //     state.hoveredNode = undefined;
+    //     state.hoveredNeighbors = undefined;
+    //   }
+
+    //   // Refresh rendering:
+    //   renderer.refresh();
+    // }
+
+    // renderer.on("enterNoode", ({ node }) => {
+    //   setHoveredNode(node);
+    // });
+
+    // renderer.on("leaveNode", () => {
+    //   setHoveredNode(undefined);
+    // });
+
+    // // Render nodes accordingly to the internal state:
+    // // 1. If a node is selected, it is highlighted
+    // // 2. If there is query, all non-matching nodes are greyed
+    // // 3. If there is a hovered node, all non-neighbor nodes are greyed
+    // renderer.setSetting("nodeReducer", (node, data) => {
+    //   const res = { ...data };
+
+    //   if (
+    //     state.hoveredNeighbors &&
+    //     !state.hoveredNeighbors.has(node) &&
+    //     state.hoveredNode !== node
+    //   ) {
+    //     res.label = "";
+    //     res.color = "#f6f6f6";
+    //   }
+
+    //   if (state.selectedNode === node) {
+    //     res.highlighted = true;
+    //   } else if (state.suggestions && !state.suggestions.has(node)) {
+    //     res.label = "";
+    //     res.color = "#f6f6f6";
+    //   }
+
+    //   return res;
+    // });
+
+    // // Render edges accordingly to the internal state:
+    // // 1. If a node is hovered, the edge is hidden if it is not connected to the
+    // //    node
+    // // 2. If there is a query, the edge is only visible if it connects two
+    // //    suggestions
+    // renderer.setSetting("edgeReducer", (edge, data) => {
+    //   const res = { ...data };
+
+    //   if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+    //     res.hidden = true;
+    //   }
+
+    //   if (
+    //     state.suggestions &&
+    //     (!state.suggestions.has(graph.source(edge)) ||
+    //       !state.suggestions.has(graph.target(edge)))
+    //   ) {
+    //     res.hidden = true;
+    //   }
+
+    //   return res;
+    // });
+
     nodes.forEach((node) => {
       graph.addNode(node.id, {
         x: Math.random(),
@@ -25,6 +107,9 @@ export const LoadGraph = ({ network }) => {
         size: 3,
         label: node.label,
         color: "#70d8bd",
+        // onClick: () => {
+        //   console.log("clicked on node", node.id);
+        // },
       });
     });
     edges.forEach((edge) => {
@@ -58,6 +143,7 @@ export const DisplayGraph = ({ width, height, network }) => {
     <div className="display-graph">
       <SigmaContainer
         className="graph-container"
+        // id="sigma-container"
         style={{ width, height, backgroundColor: "#DEE4E7" }}
       >
         <LoadGraph network={network} />
