a2a3942570831d3706c58dff408d3ab3c8886f3c
adi peled <adipeled224@gmail.com>
2023-03-06 17:24:31 +0200

    fix css issues

--- src/cmp/profileCard/profileCard.jsx
+++ src/cmp/profileCard/profileCard.jsx
@@ -9,7 +9,7 @@ export function ProfileCard(props) {
 			<header>
 				{/* <img src={avatar} alt={props.name} /> */}
 			</header>
-			<h1 className="bold-text">
+			<h1 className="title">
 				{props.name} 
 			</h1>
 			<h2 className="normal-text">{props.email}</h2>
@@ -18,11 +18,11 @@ export function ProfileCard(props) {
 			<div className="social-container">
 				<div className="followers">
 					<h1 className="bold-text">{props.processingProjects}</h1>
-					<h2 className="smaller-text">Projects in processing</h2>
+					<h2 className="smaller-text text-profile">Projects in processing</h2>
 				</div>
 				<div className="likes">
-					<h1 className="bold-text">{props.openProjects}</h1>
-					<h2 className="smaller-text">Open Projects</h2>
+					<h1 className="bold-text ">{props.openProjects}</h1>
+					<h2 className="smaller-text text-profile ">Open Projects</h2>
 				</div>
 
 			</div>
