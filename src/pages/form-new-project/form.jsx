e9bdb690ad4dd3809aaeaeef04a2f22fab10a701
Noor Haj Dawood <noorhajdawood@gmail.com>
2023-03-14 23:57:48 +0200

    small fixes

--- src/pages/form-new-project/form.jsx
+++ src/pages/form-new-project/form.jsx
@@ -28,10 +28,16 @@ export function FormNewProject() {
         name: user.name,
       };
     });
+    const keywords = values.keywords.map((key) => {
+      return {
+        keyword: key.keyword,
+      };
+    });
     const project = {
       title: values.title,
       description: values.description,
       dataset: dataset.map((user) => user.name),
+      keywords: keywords.map((key) => key.keyword),
       startDate: values.timerange[0],
       endDate: values.timerange[1],
       userEmail: values.userEmail,
@@ -49,7 +55,7 @@ export function FormNewProject() {
       dataset: [{ name: "" }],
       timerange: "",
       userEmail: "",
-      keywords: [{ keyword: "" }]
+      keywords: [{ keyword: "" }],
     },
 
     validate: (values) => {
@@ -159,27 +165,29 @@ export function FormNewProject() {
 
   const fieldsKeywords = form.values.keywords.map((_, index) => {
     // index = index +5000
-    return (<Draggable key={index+"k"} index={index} draggableId={(index).toString()}>
-      {(provided) => (
-        <Group ref={provided.innerRef} mt="xs" {...provided.draggableProps}>
-          <Group
-            position="left"
-            className="container-row"
-            {...provided.dragHandleProps}
-          >
-            <IconGridDots size={35} color="#55bbee" />
-            <TextInput
-              className="input-user"
-              onPaste={(e) => {
-                handelInputTextKeyword(e, index);
-              }}
-              placeholder="Keyword"
-              {...form.getInputProps(`keywords.${index}.keyword`)}
-            />
+    return (
+      <Draggable key={index + "k"} index={index} draggableId={index.toString()}>
+        {(provided) => (
+          <Group ref={provided.innerRef} mt="xs" {...provided.draggableProps}>
+            <Group
+              position="left"
+              className="container-row"
+              {...provided.dragHandleProps}
+            >
+              <IconGridDots size={35} color="#55bbee" />
+              <TextInput
+                className="input-user"
+                onPaste={(e) => {
+                  handelInputTextKeyword(e, index);
+                }}
+                placeholder="Keyword"
+                {...form.getInputProps(`keywords.${index}.keyword`)}
+              />
+            </Group>
           </Group>
-        </Group>
-      )}
-    </Draggable>)
+        )}
+      </Draggable>
+    );
   });
 
   const nextStep = () =>
@@ -195,200 +203,213 @@ export function FormNewProject() {
 
   return (
     <>
-    <div className="title-project title-header ">New Project</div>
-    <div className="form-new-project">
-      <div className="form-new-project-container">
-        <MantineProvider
-          theme={{
-            colors: {
-              brand: [
-                "#63BBEC",
-                "#4384a5",
-                "#63BBEC",
-                "#06AAFF",
-                "#38aae4",
-                "#78b8d8",
-                "	#38aae4",
-                "	#4384a5",
-                "#4384a5",
-                "#38aae4",
-              ],
-            },
-            primaryColor: "brand",
-          }}
-        >
-          <Stepper active={active} breakpoint="sm">
-            <Stepper.Step label="First step" description="New project settings">
-              <h3>Create new project</h3>
-              <TextInput
-                label="Title"
-                placeholder="Title"
-                {...form.getInputProps("title")}
-                pt="5px"
-                ta="start"
-              />
-              <TextInput
-                ta="start"
-                mt="md"
-                label="Description"
-                placeholder="Description"
-                {...form.getInputProps("description")}
-              />
-            </Stepper.Step>
-
-            <Stepper.Step
-              label="Second step"
-              description="Add Twitter usernames"
-            >
-              <Box>
-                <h3>Add Usernames from twitter</h3>
-                <DragDropContext
-                  onDragEnd={({ destination, source }) =>
-                    form.reorderListItem("dataset", {
-                      from: source.index,
-                      to: destination.index,
-                    })
-                  }
-                >
-                  <Droppable droppableId="dnd-list" direction="vertical">
-                    {(provided) => (
-                      <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {fields}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                </DragDropContext>
-
-                <Group position="left" mt="md">
-                  <Button
-                    mb="20px"
-                    className="add-btn"
-                    onClick={() => form.insertListItem("dataset", { name: "" })}
-                  >
-                    Add UserName
-                  </Button>
-                  <Button
-                    mb="20px"
-                    className="red-btn"
-                    onClick={() => {
-                      if (form.values.dataset.length === 1) return;
-                      form.removeListItem(
-                        "dataset",
-                        form.values.dataset.length - 1
-                      );
-                    }}
-                  >
-                    Remove UserName
-                  </Button>
-                </Group>
-              </Box>
-              <Box>
-                <h3>Add Keywords from twitter</h3>
-                <DragDropContext
-                  onDragEnd={({ destination, source }) =>
-                    form.reorderListItem("keywords", {
-                      from: source.index,
-                      to: destination.index,
-                    })
-                  }
-                >
-                  <Droppable droppableId="dnd-list" direction="vertical">
-                    {(provided) => (
-                      <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {fieldsKeywords}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                </DragDropContext>
-
-                <Group position="left" mt="md">
-                  <Button
-                    mb="20px"
-                    className="add-btn"
-                    onClick={() => form.insertListItem("keywords", { keyword: "" })}
-                  >
-                    Add Keyword
-                  </Button>
-                  <Button
-                    mb="20px"
-                    className="red-btn"
-                    onClick={() => {
-                      if (form.values.keywords.length === 1) return;
-                      form.removeListItem(
-                        "keywords",
-                        form.values.keywords.length - 1
-                      );
-                    }}
-                  >
-                    Remove keyWord
-                  </Button>
-                </Group>
-              </Box>
-            </Stepper.Step>
-
-            <Stepper.Step label="Final step" description="Pick dates range">
-              <h3>Pick start and end date</h3>
-              <DateRangePicker
-                mt="5px"
-                label="Pick dates range"
-                ta="start"
-                placeholder="Pick dates range"
-                {...form.getInputProps("timerange")}
-              />
-            </Stepper.Step>
-            <Stepper.Completed>
-              Completed! Form values:
-              <Code block mt="xl">
-                {JSON.stringify(form.values, null, 2)}
-              </Code>
-              {/* input for user email */}
-              <TextInput
-                ta="start"
-                mt="md"
-                label="Email:"
-                placeholder="Email to get notified when the project is ready"
-                {...form.getInputProps("userEmail")}
-              />
-              <Button
-                mt="xl"
-                onClick={() => {
-                  form.reset();
-                  setActive(0);
-                }}
+      <div className="title-project title-header ">New Project</div>
+      <div className="form-new-project">
+        <div className="form-new-project-container">
+          <MantineProvider
+            theme={{
+              colors: {
+                brand: [
+                  "#63BBEC",
+                  "#4384a5",
+                  "#63BBEC",
+                  "#06AAFF",
+                  "#38aae4",
+                  "#78b8d8",
+                  "	#38aae4",
+                  "	#4384a5",
+                  "#4384a5",
+                  "#38aae4",
+                ],
+              },
+              primaryColor: "brand",
+            }}
+          >
+            <Stepper active={active} breakpoint="sm">
+              <Stepper.Step
+                label="First step"
+                description="New project settings"
               >
-                Reset form
-              </Button>
-              <Button
-                mt="xl"
-                onClick={() => {
-                  if (!form.validate().hasErrors) {
-                    handleSubmission(form.values);
-                  }
-                }}
-                // type="submit"
-              >
-                Submit form
-              </Button>
-            </Stepper.Completed>
-          </Stepper>
+                <h3>Create new project</h3>
+                <TextInput
+                  label="Title"
+                  placeholder="Title"
+                  {...form.getInputProps("title")}
+                  pt="5px"
+                  ta="start"
+                />
+                <TextInput
+                  ta="start"
+                  mt="md"
+                  label="Description"
+                  placeholder="Description"
+                  {...form.getInputProps("description")}
+                />
+              </Stepper.Step>
 
-          <Group position="right" mt="xl">
-            {active !== 3 && (
-              <Button w="100%" onClick={nextStep}>
-                Next step
-              </Button>
-            )}
-            {active !== 0 && (
-              <Button w="100%" variant="default" onClick={prevStep}>
-                Back
-              </Button>
-            )}
-          </Group>
-        </MantineProvider>
+              <Stepper.Step
+                label="Second step"
+                description="Add Twitter usernames"
+              >
+                <Box>
+                  <h3>Add Usernames from twitter</h3>
+                  <DragDropContext
+                    onDragEnd={({ destination, source }) =>
+                      form.reorderListItem("dataset", {
+                        from: source.index,
+                        to: destination.index,
+                      })
+                    }
+                  >
+                    <Droppable droppableId="dnd-list" direction="vertical">
+                      {(provided) => (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {fields}
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </DragDropContext>
+
+                  <Group position="left" mt="md">
+                    <Button
+                      mb="20px"
+                      className="add-btn"
+                      onClick={() =>
+                        form.insertListItem("dataset", { name: "" })
+                      }
+                    >
+                      Add UserName
+                    </Button>
+                    <Button
+                      mb="20px"
+                      className="red-btn"
+                      onClick={() => {
+                        if (form.values.dataset.length === 1) return;
+                        form.removeListItem(
+                          "dataset",
+                          form.values.dataset.length - 1
+                        );
+                      }}
+                    >
+                      Remove UserName
+                    </Button>
+                  </Group>
+                </Box>
+                <Box>
+                  <h3>Add Keywords from twitter</h3>
+                  <DragDropContext
+                    onDragEnd={({ destination, source }) =>
+                      form.reorderListItem("keywords", {
+                        from: source.index,
+                        to: destination.index,
+                      })
+                    }
+                  >
+                    <Droppable droppableId="dnd-list" direction="vertical">
+                      {(provided) => (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {fieldsKeywords}
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </DragDropContext>
+
+                  <Group position="left" mt="md">
+                    <Button
+                      mb="20px"
+                      className="add-btn"
+                      onClick={() =>
+                        form.insertListItem("keywords", { keyword: "" })
+                      }
+                    >
+                      Add Keyword
+                    </Button>
+                    <Button
+                      mb="20px"
+                      className="red-btn"
+                      onClick={() => {
+                        if (form.values.keywords.length === 1) return;
+                        form.removeListItem(
+                          "keywords",
+                          form.values.keywords.length - 1
+                        );
+                      }}
+                    >
+                      Remove keyWord
+                    </Button>
+                  </Group>
+                </Box>
+              </Stepper.Step>
+
+              <Stepper.Step label="Final step" description="Pick dates range">
+                <h3>Pick start and end date</h3>
+                <DateRangePicker
+                  mt="5px"
+                  label="Pick dates range"
+                  ta="start"
+                  placeholder="Pick dates range"
+                  {...form.getInputProps("timerange")}
+                />
+              </Stepper.Step>
+              <Stepper.Completed>
+                Completed! Form values:
+                <Code block mt="xl">
+                  {JSON.stringify(form.values, null, 2)}
+                </Code>
+                {/* input for user email */}
+                <TextInput
+                  ta="start"
+                  mt="md"
+                  label="Email:"
+                  placeholder="Email to get notified when the project is ready"
+                  {...form.getInputProps("userEmail")}
+                />
+                <Button
+                  mt="xl"
+                  onClick={() => {
+                    form.reset();
+                    setActive(0);
+                  }}
+                >
+                  Reset form
+                </Button>
+                <Button
+                  mt="xl"
+                  onClick={() => {
+                    if (!form.validate().hasErrors) {
+                      handleSubmission(form.values);
+                    }
+                  }}
+                  // type="submit"
+                >
+                  Submit form
+                </Button>
+              </Stepper.Completed>
+            </Stepper>
+
+            <Group position="right" mt="xl">
+              {active !== 3 && (
+                <Button w="100%" onClick={nextStep}>
+                  Next step
+                </Button>
+              )}
+              {active !== 0 && (
+                <Button w="100%" variant="default" onClick={prevStep}>
+                  Back
+                </Button>
+              )}
+            </Group>
+          </MantineProvider>
+        </div>
       </div>
-    </div>
     </>
   );
 }
