370b7ebe2aa16ba9335eb3adaffd3cbfd7a25488
adi peled <adipeled224@gmail.com>
2023-03-22 14:34:48 +0200

    change date in node details card

--- src/cmp/node-details/nodeDetails.jsx
+++ src/cmp/node-details/nodeDetails.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./nodeDetails.scss";
 
 export function NodeCard({ nodeDetails }) {
+  // console.log(nodeDetails.registrationDateTwitter)
+  // // console.log(typeof(Date(nodeDetails.registrationDateTwitter).toLocaleDateString()))
+  // console.log(nodeDetails.registrationDateTwitter)
+  // if(nodeDetails.registrationDateTwitter){
+  //   nodeDetails.registrationDateTwitter = new Date(nodeDetails.registrationDateTwitter).toLocaleDateString()
+  // }
   return (
     <div className="nodeCard">
       <div className="wrapper">
@@ -16,7 +22,7 @@ export function NodeCard({ nodeDetails }) {
         <div className="about-me">{nodeDetails.description}</div>
 
         <div className="username">
-          Join Twitter At: {nodeDetails.registrationDateTwitter}
+          Join Twitter At: { new Date(nodeDetails.registrationDateTwitter).toLocaleDateString()== '1.1.1' ? 'Not Available' : new Date(nodeDetails.registrationDateTwitter).toLocaleDateString()}
         </div>
         <div className="stats">
           <div className="item followers">
